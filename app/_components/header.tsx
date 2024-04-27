98e4cc2c3fba476b8acb83d3609924754d6ec99d
Marcoant007 <marcoantnovo@gmail.com>
2024-04-27 17:36:08 -0300

    feat: refactor commit

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,23 @@
+"use client";
+
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon, icons } from "lucide-react";
+import { signIn } from "next-auth/react";
 
 const Header = () => {
+  const handleLoginClick = async () => {
+    await signIn();
+  };
   return (
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
         <Image src={"/Logo.svg"} alt="Logo" width={120} height={18} priority />
-        <Button variant={"outline"} size="icon" className="h-8 w-8"> 
+        {/* <Button variant={"outline"} size="icon" className="h-8 w-8"> 
             <MenuIcon size={16}/>
-        </Button>
+        </Button> */}
+        <Button onClick={handleLoginClick}>Login</Button>
       </CardContent>
     </Card>
   );
